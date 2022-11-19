325df54bac3b832ea7deb019460bb7e05940ce47
bobrenja <abcd43@ukr.net>
2022-11-19 18:00:05 +0200

    num3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+const ref = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+ref.textarea.addEventListener('input', onTextInputTextarea);
+ref.form.addEventListener('submit', submitComent);
+const STOREGE_KEY = ""
+readLocal();
 
+function onTextInputTextarea (e) {
+    const val = e.currentTarget.value
+
+localStorage.setItem('textarea-message', val)
+} 
+
+function readLocal() {
+   const val =  localStorage.getItem('textarea-message');
+   ref.textarea.value = val;
+}
+
+
+function submitComent(e) {
+    e.preventDefault();
+    localStorage.removeItem('textarea-message')
+    e.currentTarget.reset();
+    console.log("відправка...")
+}
