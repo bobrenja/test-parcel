a2b4ced8059688599fb0c9afcfc3825b72d559cc
bobrenja <abcd43@ukr.net>
2022-11-19 23:13:55 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,61 @@
+
+import throttle from 'lodash.throttle';
 const ref = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
 }
-ref.textarea.addEventListener('input', onTextInputTextarea);
+// ref.textarea.addEventListener('input', onTextInputTextarea);
+const STOREGE_KEY = "value-form"
 ref.form.addEventListener('submit', submitComent);
-const STOREGE_KEY = ""
+ref.form.addEventListener('input', throttle(onTextInputTextarea, 500));
+
 readLocal();
+// const a = JSON.parse(localStorage.getItem(STOREGE_KEY));
+// const formData = a ? JSON.parse(localStorage.getItem(STOREGE_KEY)) : {}
+let formData = JSON.parse(localStorage.getItem(STOREGE_KEY)) || {};
+
 
 function onTextInputTextarea (e) {
-    const val = e.currentTarget.value
-
-localStorage.setItem('textarea-message', val)
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STOREGE_KEY, JSON.stringify(formData))
 } 
 
 function readLocal() {
-   const val =  localStorage.getItem('textarea-message');
-   ref.textarea.value = val;
+
+const val = JSON.parse(localStorage.getItem(STOREGE_KEY));
+  console.log(val)
+//    ref.textarea.value = val;
+if(val) {
+
+//    val.forEach(function (number, index) {
+//     console.log(`Індекс ${index}, значення ${number}`);
+//   });
+
+const keys = Object.keys(val);
+console.log(keys)
+keys.forEach(x => {
+
+if(x === 'email' )  ref.form.email.value = val[x];
+if(x === 'message' )  ref.form.message.value = val[x];
+
+});
+
+
+} 
+
+
 }
 
 
 function submitComent(e) {
     e.preventDefault();
-    localStorage.removeItem('textarea-message')
+    localStorage.removeItem(STOREGE_KEY)
     e.currentTarget.reset();
     console.log("відправка...")
 }
+
+
+
+
+
+
