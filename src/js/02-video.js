1eeb5b7488389e03a104a7472c0529a66ec2b397
bobrenja <abcd43@ukr.net>
2022-11-20 10:24:59 +0200

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+
+    let timeLocal = localStorage.getItem('videoplayer-current-time');
+    timeLocal = timeLocal ? timeLocal : 0
+   
+        player.setCurrentTime(timeLocal).then(function(seconds) {
+            // seconds = the actual time that the player seeked to
+        }).catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                    // the time was less than 0 or greater than the video’s duration
+                   break;
+        
+                default:
+                    // some other error occurred
+                    break;
+            }
+        });
+
+    
+
+    player.on('timeupdate', throttle(data => saveTimePlayer(data), 1000)
+    ) ;
+
+  function saveTimePlayer(data) {
+    console.log(data)
+    localStorage.setItem('videoplayer-current-time', data['seconds'] )
+  }
